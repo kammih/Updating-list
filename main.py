0db5cd349d608eb035bd6cdffd1a43fb4e48c624
Kammi Henderson <kammi_henry@yahoo.com>
2024-11-11 15:10:36 -0600

    Slicing

---
+++ main.py
@@ -0,0 +1,11 @@
+names = ["Rubi", "Kate", "Truitt", "Daniel", "Tyler", "Clay", "Kasi", "Cassie", "Doug", "Bryce", "Amber", "Alyssa", "Kyle", "Kassi", "Shae", "Victoria", "Krystal", "Alex", "Emily", "Anna"]
+
+section1 = names[:4]
+section2 = names[16:]
+section3 = names[10:14]
+section4 = names[4]
+
+print(section1)
+print(section2)
+print(section3)
+print(section4)
